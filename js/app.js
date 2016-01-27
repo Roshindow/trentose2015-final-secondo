1734bf863c60495cc21af62fd13fd5839f2d2f83
Roshindow <michelienrico@hotmail.it>
2016-01-27 15:52:14 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
+var score=0;
+
 var FlirtrModel = {  
   
    /* It sets the phrase to analyse */
    analyse : function (phrase){
+	   FlirtrModel.getScore(phrase);
    },
   
    /* Returns the flirting score (number) of the phrase.
@@ -10,7 +13,29 @@ var FlirtrModel = {
     * patterns (unless the pattern neutraliser is found to 
     * the right of the matching word)
     */
-   getScore : function () {
+   getScore : function (str) {
+	   var str2="";
+	   var s=0;
+	   for (var i = 0; i < patterns.length; i++) {
+			var n = str.search(patterns[i].word);
+			if(n>=0){
+				str2 = str.substr(n+(patterns[i].word.length),str.length);
+				var m = str2.search(patterns[i].neutraliser);
+					if(m<0){
+						s+=patterns[i].score;
+					}
+			}
+	   }
+	   score = s;
    }
          
 };
+
+function esegui(){
+	x = document.getElementById("phrase").value;
+	FlirtrModel.analyse(x);
+	$(".answer").empty();
+	$(".answer").append("Score: ");
+	//document.getElementById("answer").innerHTM = "Score =";
+	$(".answer").append(score);
+}
